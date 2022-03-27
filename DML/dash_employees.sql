4c24730c3ec0bc36e83f75d1896c5792a3195ce4
Alcarstaris <75307645+Ivana338@users.noreply.github.com>
2022-03-27 20:17:24 +0200

    DML

---
+++ DML/dash_employees.sql
@@ -0,0 +1,11 @@
+INSERT INTO dash.employees (employee_id, firstname, lastname, date_of_birth)
+VALUES  (1, 'James', 'Henry', '1943-02-27 00:00:00'),
+        (2, 'Robert', 'Walsh', '1908-01-19 00:00:00'),
+        (3, 'Sharon', 'Pruitt', '1955-07-01 00:00:00'),
+        (4, 'Samuel', 'Thompson', '1909-11-14 00:00:00'),
+        (5, 'Alexis', 'Schmidt', '1914-06-16 00:00:00'),
+        (6, 'Jessica', 'Scott', '1914-04-03 00:00:00'),
+        (7, 'Danielle', 'Johns', '1952-12-22 00:00:00'),
+        (8, 'Mario', 'Lee', '1999-08-17 00:00:00'),
+        (9, 'Patrick', 'Baker', '1914-08-17 00:00:00'),
+        (10, 'Allison', 'Hale', '1921-03-27 00:00:00');
